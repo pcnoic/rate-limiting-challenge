4f419096dc9f5c8780c094c973ba7845de3c9cc0
Christos Alexiou <pcnoic@gmail.com>
2022-03-19 11:17:56 +0200

    Update throttling tests

--- test_throttling.sh
+++ test_throttling.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-for i in {0..100}; do 
+echo "Testing throttling per sliding window."
+for i in {0..50}; do 
     curl  -s -o /dev/null -H "Token: abcdef" -w "%{http_code}\n" "http://localhost:8000/up"
 done
+echo "Sleeping for 10 seconds"
+sleep 10
+echo "Testing throttling per sliding window again"
+for i in {0..50}; do 
+    curl  -s -o /dev/null -H "Token: abcdef" -w "%{http_code}\n" "http://localhost:8000/up"
+done
+echo "Test finished!"
+sleep 5
+echo "Testing throttling per User Token"
+for i in {0..50}; do 
+    echo "User 1: abcdef"
+    curl  -s -o /dev/null -H "Token: abcdef" -w "%{http_code}\n" "http://localhost:8000/up"
+    echo "User 2: fedcba"
+    curl  -s -o /dev/null -H "Token: fedcba" -w "%{http_code}\n" "http://localhost:8000/up"
+    echo "User 3: blabla"
+    curl  -s -o /dev/null -H "Token: blabla" -w "%{http_code}\n" "http://localhost:8000/up"
+done
+echo "Test finshed!"
